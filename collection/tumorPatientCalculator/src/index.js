58957d9ce7e211d35d4facf66697e3e1a003255d
George Meng <gqmeng.meng@gmail.com>
2020-04-02 15:52:54 -0400

    Update tumorPatientCalculator and test

--- collection/tumorPatientCalculator/src/index.js
+++ collection/tumorPatientCalculator/src/index.js
@@ -1,14 +1,19 @@
 function cnstap(inputs){
   // inputs = pathway, genonmic indicators
-
   var pathway = inputs.pathway
   var gi1 = inputs.percent_tumor
   var gi2 = inputs.parsons_score
 
-
-  //
-
-
+  const weights={
+    clonality:5,
+    tierscore:3
+  }
+  var results = inputs
+  if(gi1==null | gi2==null) {     // Missing input field
+    results.error="Invalid Input."
+  }else{
+    results.subtotal=gi1*weights.clonality+gi2*weights.tierscore
+  }
   return results
 
 }
--- test/tumorPatientCalculator.test.js
+++ test/tumorPatientCalculator.test.js
@@ -7,27 +7,19 @@ var javascript = rewire("../collection/tumorPatientCalculator/src/index");
 //THIS VARIABLE NEEDS A FUNCTION NAME FROM THE KO OF INTEREST
 var table = javascript.__get__("cnstap");
 
-
 // TEST 1 - Pathway "ALK"; clonality = 1; varient_tier = 2
 var input = {"pathway":"ALK",percent_tumor:1,parsons_score:2}
 
 test('Normal Input', () => {
-
     // shows the corresponding drugs to pathway
     var result = table(input);
-    console.log(JSON.stringify(result, undefined, 2));
+    console.log(result)
+    expect(result.subtotal).toEqual(11)
+});
 
-    for(var i in result){
-
-      console.log(JSON.stringify(result[i], undefined, 2));
-
-      // new total = previous total + clonality*weight + varient_tier*weight
-      result[i].total_points = result[i].total_points + 1*5 + 2*3;
-
-      var score = result[i].total_points;
-      console.log(JSON.stringify(result[i].drug_name));
-      console.log(JSON.stringify(result[i].total_points));
-
-    }
-
+test('Missing paramter Input', () => {
+    // shows the corresponding drugs to pathway
+    var result = table({"pathway":"ALK",percent_tumor:1});
+    console.log(result)
+    expect(result.error).toEqual("Invalid Input.")
 });
