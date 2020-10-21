5dc4af5faa0d27745ea8f211dab5da4d12202d39
ukarthik123 <53403086+ukarthik123@users.noreply.github.com>
2020-10-21 12:37:25 -0400

    Add files via upload

--- collection/CNSTAPTPC/tumorPatientCalculator/v2.0/src/index.js
+++ collection/CNSTAPTPC/tumorPatientCalculator/v2.0/src/index.js
@@ -1,19 +1,13 @@
-function cnstap(inputs){
-  // inputs = pathway, genonmic indicators
-  var pathway = inputs.pathway
-  var gi1 = inputs.percent_tumor
-  var gi2 = inputs.parsons_score
+function cnstap(){
+
 
   var weights={
-    clonality:5,
-    tierscore:3
+    clonalityweight:5,
+    tierscoreweight:3,
+    trialweight:20,
+
   }
-  var results = inputs
-  if(gi1==null | gi2==null) {     // Missing input field
-    results.error="Invalid Input."
-  }else{
-    results.subtotal=gi1*weights.clonality+gi2*weights.tierscore
-  }
-  return results
+
+  return JSON.stringify(weights);
 
 }
